ae95a85434408a74eeb55516b29f29be51264b56
ChrisferDev <ferchris82@hotmail.com>
2024-06-12 16:39:29 -0500

    Versión 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HeaderAdminComponent } from './components/header-admin/header-admin.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes : Routes =[
+  {path:'', component:HomeComponent},
+  {path:'admin/product', component: ProductListComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductListComponent,
+    HeaderAdminComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
     
   ],
   providers: [],
